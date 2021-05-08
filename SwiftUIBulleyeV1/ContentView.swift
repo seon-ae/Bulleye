9aaa053533bae539bc95c43763a937fac8a1a6f1
seonae <vkdtjsdo@gmail.com>
2021-05-09 03:15:22 +0900

    Initial Commit

---
+++ SwiftUIBulleyeV1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIBulleyeV1
+//
+//  Created by 황선애 on 2021/05/09.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
